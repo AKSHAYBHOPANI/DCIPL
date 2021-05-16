af5dd4602371cf10a23571f9c27e6a765c5379ca
unknown <kalyangoud288@gmail.com>
2021-05-16 11:56:08 +0530

    Updated Careers Page

--- src/Main.js
+++ src/Main.js
@@ -5,6 +5,7 @@ import Home from './Components/Home';
 import login from './Components/login';
 import register from './Components/register';
 import dashboard from './Components/dashboard';
+import Careers from './Components/Careers';
 
 const Main = () => {
   return (
@@ -13,6 +14,7 @@ const Main = () => {
       <Route exact path='/login' component={login}></Route>
       <Route exact path='/register' component={register}></Route>
       <Route exact path='/dashboard' component={dashboard}></Route>
+      <Route exact path='/Careers' component={Careers}></Route>
     </Switch>
   );
 }
