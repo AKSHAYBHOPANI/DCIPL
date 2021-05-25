68adabd2cdc2aed2379ecab991747ea173dd4815
Jyoti Sharma <83448327+JYOTICREATION@users.noreply.github.com>
2021-05-25 23:49:30 +0530

    Changed as earlier code

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -17,12 +17,12 @@ function Nav({IsSignIn, setIsSignIn}) {
     {IsSignIn ? (
 
   <>
-  <div className="header">
+  <header className="header">
   <a href="./"><img src={logo} className="logo" alt=""></img></a>
   <input className="menu-btn" type="checkbox" id="menu-btn" />
   <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
-		<div className="menu">
-  <ul>
+		
+  <ul className="menu">
   <li><a href="../">Home</a></li>
     <li><a href="../about">About</a></li>
     <li><a href="../careers">Careers</a></li>
@@ -31,7 +31,7 @@ function Nav({IsSignIn, setIsSignIn}) {
   </ul>
 </div>
 
-</div>
+</header>
 </>
       ) : (
 
