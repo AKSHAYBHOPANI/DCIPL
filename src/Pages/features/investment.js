9cacc840e37ec98c01f7be7941cc44f5fd0c24f1
Akshay Bhopani <akshaybhopani@gmail.com>
2021-05-27 16:41:59 +0530

    Data Population In Investment page

--- src/Pages/features/investment.js
+++ src/Pages/features/investment.js
@@ -7,7 +7,20 @@ import Login from '../login';
 function Investment({IsSignIn,setIsSignIn}) {
 
 const [FixedIncome, setFixedIncome] = useState("");
-const [OtherIncome, setOtherIncome] = useState("");      
+const [OtherIncome, setOtherIncome] = useState("");
+const [MedianIncome, setMedianIncome] = useState("");
+const [TotalExpenses, setTotalExpenses] = useState("");
+const [SavingsIncome, setSavingsIncome] = useState("");
+const [Age, setAge] = useState("");
+const [RetirementAge, setRetirementAge] = useState("");
+const [AssestClass, setAssestClass] = useState("");
+const [Return, setReturn] = useState("");   
+const [Risk, setRisk] = useState("");  
+const [Time, setTime] = useState("");
+const [FinancialRisk, setFinancialRisk] = useState("");
+const [Standard, setStandard] = useState("");
+const [RiskWillingness, setRiskWillingness] = useState("");
+const [Liquidity, setLiquidity] = useState("");     
   
  const FixedIncomeValue = (event) => {
   setFixedIncome(event.target.value);
@@ -17,13 +30,69 @@ const [OtherIncome, setOtherIncome] = useState("");
   setOtherIncome(event.target.value);
   };
 
+  const MedianIncomeValue = (event) => {
+  setMedianIncome(event.target.value);
+  };
+
+  const TotalExpensesValue = (event) => {
+  setTotalExpenses(event.target.value);
+  };
+
+  const SavingsIncomeValue = (event) => {
+  setSavingsIncome(event.target.value);
+  };
+  const AgeValue = (event) => {
+  setAge(event.target.value);
+  };
+  const RetirementAgeValue = (event) => {
+  setRetirementAge(event.target.value);
+  };
+  const AssestClassValue = (event) => {
+  setAssestClass(event.target.value);
+  };
+  const ReturnValue = (event) => {
+  setReturn(event.target.value);
+  };
+  const RiskValue = (event) => {
+  setRisk(event.target.value);
+  };
+  const TimeValue = (event) => {
+  setTime(event.target.value);
+  };
+  const FinancialRiskValue = (event) => {
+  setFinancialRisk(event.target.value);
+  };
+  const StandardValue = (event) => {
+  setStandard(event.target.value);
+  };
+  const RiskWillingnessValue = (event) => {
+  setRiskWillingness(event.target.value);
+  };
+  const LiquidityValue = (event) => {
+  setLiquidity(event.target.value);
+  };
+
 const onSubmitSignIn = () => {
     fetch('https://dcipl.yourtechshow.com/investment', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
         FixedIncome: FixedIncome,
-        OtherIncome: OtherIncome
+        OtherIncome: OtherIncome,
+        MedianIncome: MedianIncome,
+        TotalExpenses: TotalExpenses,
+        SavingsIncome: SavingsIncome,
+        Age: Age,
+        RetirementAge: RetirementAge,
+        AssestClass: AssestClass,
+        Return: Return,
+        Risk: Risk,
+        Time: Time,
+        FinancialRisk: FinancialRisk,
+        Standard: Standard,
+        RiskWillingness: RiskWillingness,
+        Liquidity: Liquidity
+
       })
     })
       
@@ -34,9 +103,19 @@ const onSubmitSignIn = () => {
        console.log(FS)
        var table = ""
        let x = ""
-        table += "<tr><td>" + "Good =" + FS.Good + "</td></tr>";
-        table += "<tr><td>" + "Average =" + FS.Average + "</td></tr>";
-        table += "<tr><td>" + "Bad =" + FS.Bad + "</td></tr>";
+        table += "<tr><td>" + "Total Income =" + FS.TotalIncome + "</td></tr>";
+        table += "<tr><td>" + "Median Income =" + FS.MedianIncome + "</td></tr>";
+        table += "<tr><td>" + "Total Expenses =" + FS.TotalExpenses + "</td></tr>";
+        table += "<tr><td>" + "Savings Income =" + FS.SavingsIncome + "</td></tr>";
+        table += "<tr><td>" + "Age =" + FS.Age + "</td></tr>";
+        table += "<tr><td>" + "Retirement Age =" + FS.RetirementAge + "</td></tr>";
+        table += "<tr><td>" + "Asset Class =" + FS.AssestClass + "</td></tr>";
+        table += "<tr><td>" + "Return =" + FS.Return + "</td></tr>";
+        table += "<tr><td>" + "Time=" + FS.Time + "</td></tr>";
+        table += "<tr><td>" + "Financial Risk=" + FS.FinancialRisk + "</td></tr>";
+        table += "<tr><td>" + "Standard of Living=" + FS.Standard + "</td></tr>";
+        table += "<tr><td>" + "Risk Willingness=" + FS.RiskWillingness + "</td></tr>";
+        table += "<tr><td>" + "Liquidity=" + FS.Liquidity + "</td></tr>";
        document.getElementById("table").innerHTML = table;
       })
   }
@@ -53,11 +132,50 @@ const onSubmitSignIn = () => {
                         <br></br>
                         <input type="text" name="fixed-income" placeholder="Fixed Income (Per Month)" required onChange={FixedIncomeValue} value={FixedIncome}></input><br></br>
                         <input type="text" name="fixed-income" placeholder="Other Income (Per Month)" required onChange={OtherIncomeValue} value={OtherIncome}></input><br></br>
-                    </form> 
+                        <input type="text" name="fixed-income" placeholder="Median Income (Per Month)" required onChange={MedianIncomeValue} value={MedianIncome}></input><br></br>
+                        <input type="text" name="fixed-income" placeholder="Total Expenses (Per Month)" required onChange={TotalExpensesValue} value={TotalExpenses}></input><br></br>
+                        <input type="text" name="fixed-income" placeholder="Income Available For Investment (Savings)" required onChange={SavingsIncomeValue} value={SavingsIncome}></input><br></br>
+                        <input type="text" name="fixed-income" placeholder="Current Age" required onChange={AgeValue} value={Age}></input><br></br>
+                        <input type="text" name="fixed-income" placeholder="Retirement Age Proposed" required onChange={RetirementAgeValue} value={RetirementAge}></input><br></br>
+                        <input type="text" name="fixed-income" placeholder="Assest Class" required onChange={AssestClassValue} value={AssestClass}></input><br></br>
+                        <input type="text" name="fixed-income" placeholder="Return" required onChange={ReturnValue} value={Return}></input><br></br>
+                        <input type="text" name="fixed-income" placeholder="Risk" required onChange={RiskValue} value={Risk}></input><br></br>
+                        <input type="text" name="fixed-income" placeholder="Time Duration For Investment (In Years)" required onChange={TimeValue} value={Time}></input><br></br>
+                        <label>Financial Risk - </label>
+                          <select onChange={FinancialRiskValue} value={FinancialRisk}>
+                          <option value="">Select</option>
+                            <option value="High">High</option>
+                            <option value="Medium">Medium</option>
+                            <option value="Low">Low</option>
+                          </select>
+                          <label>  Standard Of Living - </label>
+                          <select onChange={StandardValue} value={Standard}>
+                           <option value="">Select</option>
+                            <option value="High">High</option>
+                            <option value="Medium">Medium</option>
+                            <option value="Low">Low</option>
+                          </select>
+                          <label>  Risk Willingness - </label>
+                          <select onChange={RiskWillingnessValue} value={RiskWillingness} >
+                           <option value="">Select</option>
+                            <option value="High">High</option>
+                            <option value="Medium">Medium</option>
+                            <option value="Low">Low</option>
+                          </select>
+                          <label>  Liquidity - </label>
+                          <select onChange={LiquidityValue} value={Liquidity}>
+                           <option value="">Select</option>
+                            <option value="High">High</option>
+                            <option value="Medium">Medium</option>
+                            <option value="Low">Low</option>
+                          </select>
+                        </form> <br></br>
                     <button type="submit" onClick={onSubmitSignIn}>Calculate</button>
 </div> 
 <h2>Financial Stability</h2>
-<table id="table"/>        
+<table id="table"/> 
+
+<br></br>   <br></br> <br></br>  
 </>
       ) : (
   <Login IsSignIn={IsSignIn} setIsSignIn={setIsSignIn} />
