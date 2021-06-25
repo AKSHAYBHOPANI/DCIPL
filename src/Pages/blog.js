780a0a3f7c9efd763fb8acaaea7ecdd2e66e5dcb
Jyoti Sharma <jyotijyotisharma95@yahoo.com>
2021-06-25 15:52:45 +0530

    solved css issues of bottom

--- src/Pages/blog.js
+++ src/Pages/blog.js
@@ -1,12 +1,18 @@
 import React from 'react';// JavaScript Document
 import './CSS/blog.css';
+import video from "../assests/finance.mp4";
 
 
 function blog() {
 	return(
 	<>
 
+<div className="blog1">
 
+<video src={video}  autoPlay="true" /> 
+            
+
+</div>
 
 
 
