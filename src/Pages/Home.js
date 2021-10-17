0663f721d4376918c1de74537fe1fe005bde44bf
Jyoti Sharma <jyotijyotisharma95@yahoo.com>
2021-10-18 00:43:27 +0530

    fixed bugs

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -139,37 +139,37 @@ const Home = () => {
   ];
 
   // Statistics
-  var project = setInterval(projectDone, 10);
-  var clients = setInterval(happyClients, 10);
-  var service = setInterval(service, 10);
+  // var project = setInterval(projectDone, 10);
+  // var clients = setInterval(happyClients, 10);
+  // var service = setInterval(service, 10);
 
-  let count1 = 1;
-  let count2 = 1;
-  let count3 = 1;
+  // let count1 = 1;
+  // let count2 = 1;
+  // let count3 = 1;
 
-  function projectDone() {
-    count1++;
-    document.querySelector("#number1").innerHTML = count1;
-    if (count1 == 500) {
-      clearInterval(project);
-    }
-  }
+  // function projectDone() {
+  //   count1++;
+  //   document.querySelector("#number1").innerHTML = count1;
+  //   if (count1 == 500) {
+  //     clearInterval(project);
+  //   }
+  // }
 
-  function happyClients() {
-    count2++;
-    document.querySelector("#number2").innerHTML = count2;
-    if (count2 == 89) {
-      clearInterval(clients);
-    }
-  }
+  // function happyClients() {
+  //   count2++;
+  //   document.querySelector("#number2").innerHTML = count2;
+  //   if (count2 == 89) {
+  //     clearInterval(clients);
+  //   }
+  // }
 
-  function service() {
-    count3++;
-    document.querySelector("#number3").innerHTML = count3;
-    if (count3 == 359) {
-      clearInterval(service);
-    }
-  }
+  // function service() {
+  //   count3++;
+  //   document.querySelector("#number3").innerHTML = count3;
+  //   if (count3 == 359) {
+  //     clearInterval(service);
+  //   }
+  // }
 
   return (
     <>
